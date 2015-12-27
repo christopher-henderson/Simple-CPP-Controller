b00af17c08414b277d056b58e0b471236c488a14
christopher-henderson <chris.henderson@saifeinc.com>
2015-12-27 14:37:41 -0700

    Now dropping use of submodules in favor of scripting.

--- install.sh
+++ install.sh
@@ -15,25 +15,37 @@ BOOST_VERSION=1_60_0
 # }
 
 apt-get update
-apt-get install git gcc g++ cmake libicu-dev
+apt-get install gcc g++ cmake libicu-dev -y
 
-wget http://skylineservers.dl.sourceforge.net/project/boost/boost/1.60.0/boost_$BOOST_VERSION.tar.gz && tar zxvf boost_$BOOST_VERSION.tar.gz
-cd boost_$BOOST_VERSION
+#wget http://skylineservers.dl.sourceforge.net/project/boost/boost/1.60.0/boost_$BOOST_VERSION.tar.gz && tar zxvf boost_$BOOST_VERSION.tar.gz
+#cd boost_$BOOST_VERSION
+git clone https://github.com/boostorg/boost.git
+cd deps/boost
+git submodule init libs/program_options
+git submodule init libs/test
+git submodule init libs/system
+git submodule init libs/regex
+git submodule init libs/date_time
+git submodule init libs/thread
+git submodule init libs/filesystem
+git submodule init libs/chrono
+git submodule init libs/atomic
+git submodule update
 ./bootstrap.sh --with-libraries=program_options,test,system,regex,date_time,thread,filesystem,chrono,atomic
 sudo ./b2 install
-cd ~
+
+cd ..
 
 git clone https://github.com/cpp-netlib/cpp-netlib.git
-cd cpp-netlib
-git submodule init && git submodule update
-mkdir ~/cpp-netlib-build
-cd ~/cpp-netlib-build
-cmake ~/cpp-netlib
-make -j4 && make install
-cd ~
+mkdir cpp-netlib-build
+cd cpp-netlib-build
+cmake ../cpp-netlib
+make -j2 && make install
+cd ..
+rm -rf cpp-netlib-build
 
 
-git clone https://github.com/christopher-henderson/Simple-CPP-Server.git
-cd Simple-CPP-Server/example
-export BOOST_ROOT=/usr/local/include
-g++ server.cpp -I$BOOST_ROOT -I. -I$BOOST_ROOT/stage/lib -L/usr/local/lib -lboost_system -ljsoncpp -pthread
+# git clone https://github.com/christopher-henderson/Simple-CPP-Server.git
+# cd Simple-CPP-Server/example
+# export BOOST_ROOT=/usr/local/include
+# g++ server.cpp -I$BOOST_ROOT -I. -I$BOOST_ROOT/stage/lib -L/usr/local/lib -lboost_system -ljsoncpp -pthread
